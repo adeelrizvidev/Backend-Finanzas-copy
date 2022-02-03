76e8342aaf9854cf9d5404596a57b91cb1238f0b
Adeel Rizvi <adeelrizvi89@gmail.com>
2022-02-03 18:05:46 +0500

    browser.js edit

--- browser.js
+++ browser.js
@@ -1,21 +1,22 @@
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer');
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer");
 
-async function startBrowser(){
-    let browser;
-    try {
-        console.log("Opening the browser......");
-        browser = await chromium.puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox'] ,
-            'ignoreHTTPSErrors': true
-        });
-    } catch (err) {
-        console.log("Could not create a browser instance => : ", err);
-    }
-    return browser;
+async function startBrowser() {
+  let browser;
+  try {
+    console.log("Opening the browser......");
+    browser = await chromium.puppeteer.launch({
+      headless: true,
+      defaultViewport: null,
+      args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote"],
+      ignoreHTTPSErrors: true,
+    });
+  } catch (err) {
+    console.log("Could not create a browser instance => : ", err);
+  }
+  return browser;
 }
 
 module.exports = {
-    startBrowser
+  startBrowser,
 };
